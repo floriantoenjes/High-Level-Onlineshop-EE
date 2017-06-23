7e35b1fe1cca7bd9c7429e9a1d5a570b361528e7
Florian Tönjes <skatepainter@yahoo.de>
2017-06-23 14:55:14 +0200

    Initial commit

---
+++ src/de/java2enterprise/onlineshop/RegisterController.java
@@ -0,0 +1,45 @@
+package de.java2enterprise.onlineshop;
+
+import de.java2enterprise.onlineshop.model.Customer;
+
+import javax.annotation.Resource;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
+import javax.transaction.UserTransaction;
+
+@Named
+@RequestScoped
+public class RegisterController {
+
+    @PersistenceUnit()
+    private EntityManagerFactory emf;
+
+    @Resource
+    private UserTransaction ut;
+
+    @Inject
+    private Customer customer;
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public String persist() {
+        try {
+            ut.begin();
+            emf.createEntityManager().persist(customer);
+            ut.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "/register.xhtml";
+    }
+}
